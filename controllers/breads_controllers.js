7a8e707601addf6f4cd5f0cb49ed97fb31ec6719
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-01-13 21:52:40 -0500

    added bread1 route

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -2,7 +2,7 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
+/// INDEX
 breads.get('/', (req, res) => {
   res.send(Bread)
 })
